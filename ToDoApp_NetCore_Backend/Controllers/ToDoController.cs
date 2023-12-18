857e33d405e69e4a513faebd3596a818497d848f
baref0ot <Mattwright700@yahoo.com>
2023-12-18 15:54:27 -0500

    Added a Controller to configure API calls for C.R.D To Do Items.

---
+++ ToDoApp_NetCore_Backend/Controllers/ToDoController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoApp_NetCore_Backend.Controllers {
+    public class ToDoController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
