ac10f67087ae3d035619967dd14f3168c891f2b7
baref0ot <Mattwright700@yahoo.com>
2023-12-18 20:13:10 -0500

    Installed Microsoft.Data.SqlClient Nuget - Replaces old System.Data.SqlClient. - Needed for running Sql Commands

--- ToDoApp_NetCore_Backend/Controllers/ToDoController.cs
+++ ToDoApp_NetCore_Backend/Controllers/ToDoController.cs
@@ -1,7 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
 
 namespace ToDoApp_NetCore_Backend.Controllers {
     public class ToDoController : Controller {
+
+
+        private IConfiguration _configuration;
+
+        public ToDoController(IConfiguration configuration) {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("GetNotes")]
+        public JsonResult GetNotes() {
+
+            string query = "select * from dbo.notes";
+            DataTable dataTable = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("ToDoDBCon");
+
+            SqlDataReader myReader;
+
+
+            return Json();
+        }
+
         public IActionResult Index() {
             return View();
         }
