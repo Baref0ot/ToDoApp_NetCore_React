ef1c97aefbe9543563490067122150721a0be936
baref0ot <Mattwright700@yahoo.com>
2023-12-19 00:36:03 -0500

    Consume the .NET Core back-end Get API with React "refreshNotes()" method that fetches the request on page load.

--- React_UI_Frontend/todoapp/src/App.js
+++ React_UI_Frontend/todoapp/src/App.js
@@ -1,13 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+
+class App extends Component{
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      notes : []
+    }
+  }
 
 
-function App() {
+
+  API_URL = "http://localhost:42712/";
+
+
+  // lifecycle method that is called when page is loaded
+  componentDidMount(){
+    this.refreshNotes();
+  }
+
+
+
+  async refreshNotes(){
+    fetch(this.API_URL+"GetNotes").then(response => response.json())
+    .then(data => {
+      this.setState({notes:data});
+    })
+  }
+
+
+ render() {
+  const{notes}=this.state;
+
   return (
     <div className="App">
       <h2>To Do App</h2>
+      {notes.map(note =>
+        <p>
+          <b>* {note.description}</b>
+        </p>)}
     </div>
   );
 }
-
+}
 export default App;
